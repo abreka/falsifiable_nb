e8cb6d9b8ac2ebda37a4fbb8b9f61fb0f4e118a1
generativist <jbn@abreka.com>
2022-10-01 11:07:48 -0700

    Add file watch, serve, and open browser

--- falsifiable_nb/__init__.py
+++ falsifiable_nb/__init__.py
@@ -1,1 +1,93 @@
+import time
+from pathlib import Path
+
+import http.server
+import socketserver
+import click
+
 from falsifiable_nb.html_exporter import FalsifiableNB
+import nbformat
+import webbrowser
+from watchdog.events import FileSystemEventHandler
+
+
+def generate_html(notebook_path: Path, output_dir: Path) -> Path:
+    # Read the notebook
+    with notebook_path.open() as f:
+        nb = nbformat.read(f, as_version=4)
+
+    # Create the exporter
+    exporter = FalsifiableNB()
+    body, resources = exporter.from_notebook_node(nb)
+
+    # Write the output
+    output_path = output_dir / notebook_path.with_suffix(".html").name
+    with output_path.open("w") as f:
+        f.write(body)
+
+    # Return the location
+    return output_path
+
+
+def open_file_in_browser(file_path: Path):
+    # Get the default web browser
+    # TODO: Verify this works on Windows and MacOS
+    # TODO: Fix this so it doesn't hang
+
+    # populate _tryorder
+    # x = webbrowser.get()
+    # from webbrowser import _tryorder, _browsers
+    # for i, browser in enumerate(_tryorder):
+    #     # _tryorder[i] = browser + " &"
+    #     print(_browsers[browser])
+
+    browser = webbrowser.get()
+    browser.open(file_path.absolute().as_uri(), autoraise=False)
+
+
+def echo_generated(notebook_path: Path, output_path: Path):
+    click.echo(f"Generated {output_path}")
+
+
+class SingleNotebookChangedHandler(FileSystemEventHandler):
+    def __init__(self, notebook_path: Path, output_dir: Path, done_fn: echo_generated):
+        self._notebook_path = notebook_path
+        self._output_dir = output_dir
+        self._done_fn = done_fn
+
+    def on_modified(self, event):
+
+        if event.src_path == str(self._notebook_path):
+            # TODO: this is a horrible kludge. The modified event is triggered
+            # on start to write. It is often the case that this modification event
+            # fires and calls generate_html before the file is fully written!
+            time.sleep(0.1)
+            output_path = generate_html(self._notebook_path, self._output_dir)
+            self._done_fn(self._notebook_path, output_path)
+
+
+def serve_dir(
+    dir_path: Path, initial_port: int = 8000, fail_on_first_bad_port: bool = False
+):
+    # Create a simple http request handler with the given directory as the root
+    class Handler(http.server.SimpleHTTPRequestHandler):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, directory=str(dir_path), **kwargs)
+
+    port = initial_port
+    for i in range(100):
+        try:
+            httpd = socketserver.TCPServer(("", port), Handler)
+            click.echo(f"Serving at http://localhost:{port}")
+
+            # Serve forever in a new thread
+            import threading
+
+            t = threading.Thread(target=httpd.serve_forever, daemon=True).start()
+            return t
+        except OSError as e:
+            # TODO: verify this works on Windows and MacOS
+            if fail_on_first_bad_port or "in use" not in str(e):
+                raise
+            else:
+                port += 1
--- falsifiable_nb/cli.py
+++ falsifiable_nb/cli.py
@@ -1,4 +1,17 @@
+import time
+from pathlib import Path
+
 import click
+
+from falsifiable_nb import (
+    generate_html,
+    open_file_in_browser,
+    SingleNotebookChangedHandler,
+    echo_generated,
+    serve_dir,
+)
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
 
 
 @click.group()
@@ -7,17 +20,58 @@ def cli():
 
 
 @cli.command()
-@click.option("--output-dir", default="", help="Path to the output directory")
+@click.option(
+    "--output-dir", default="", help="Path to the output directory", type=click.Path()
+)
 @click.option("--watch", is_flag=True, help="Watch and regenerate continuously")
-@click.option("--serve", is_flag=False, help="Serve the generated file")
+@click.option("--serve", is_flag=True, help="Serve the generated file")
+@click.option("--open-browser", is_flag=True, help="Open in browser after generation")
 @click.argument("notebook_path", type=click.Path(exists=True))
 def generate(
-    output_path,
+    output_dir,
     watch,
     serve,
+    open_browser,
     notebook_path,
 ):
-    click.echo(f"Generating {notebook_path} to {output_path}")
+    notebook_path = Path(notebook_path)
+    if not output_dir:
+        output_dir = notebook_path.parent
+
+    output_path = _generate(notebook_path, output_dir)
+
+    if open_browser:
+        open_file_in_browser(output_path)
+
+    httpd = None
+    if serve:
+        httpd = serve_dir(output_dir)
+
+    if watch:
+        click.echo("Watching for changes...")
+        observer = Observer()
+        observer.schedule(
+            SingleNotebookChangedHandler(notebook_path, output_dir, echo_generated),
+            str(notebook_path.parent),
+            recursive=False,
+        )
+        click.echo("Press Ctrl+C to stop")
+        observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
+
+    if httpd:
+        httpd.shutdown()
+
+
+def _generate(notebook_path, output_dir):
+    output_path = generate_html(notebook_path, output_dir)
+    echo_generated(notebook_path, output_path)
+    return output_path
 
 
 if __name__ == "__main__":

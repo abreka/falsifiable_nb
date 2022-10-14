860e8f515e7748a3592dfd2906143708a40a3a64
Johnny Nelson <jbn@abreka.com>
2022-10-14 08:07:16 -0700

    Add github actions

--- falsifiable_nb/generate.py
+++ falsifiable_nb/generate.py
@@ -1,13 +1,13 @@
 import sys
-import time
+import threading
 from pathlib import Path
+import time
 
 import click
 import nbformat
-from nbconvert.preprocessors import TagRemovePreprocessor
 from traitlets.config import Config
 from watchdog.events import FileSystemEventHandler
-
+from threading import Thread
 from falsifiable_nb import FalsifiableNB
 
 
@@ -70,42 +70,67 @@ def echo_generated(notebook_path: Path, output_path: Path):
 
 
 class SingleNotebookChangedHandler(FileSystemEventHandler):
-    def __init__(self, notebook_path: Path, output_dir: Path, done_fn: echo_generated):
+    def __init__(
+        self,
+        notebook_path: Path,
+        output_dir: Path,
+        done_fn: echo_generated,
+        debounce_time=0.1,
+    ):
         self._notebook_path = notebook_path
         self._output_dir = output_dir
         self._done_fn = done_fn
+        self._debouncer = Debouncer(self._handle_event, debounce_time)
 
-    if sys.platform == "darwin":
+    def stop(self):
+        self._debouncer.stop()
 
-        def on_modified(self, event):
-            # TODO: doesn't fix multiple hit problem.
-            if event.src_path == str(self._notebook_path):
-                self._handle_event(event)
-
-    else:
-
-        def on_closed(self, event):
-            # Used closed rather than modified because modified is called multiple times.
-            # I think the jupyter behavior is consistent in that it will open, write, close
-            # for every save.
-            if event.src_path == str(self._notebook_path):
-                self._handle_event(event)
+    def on_modified(self, event):
+        # TODO: doesn't fix multiple hit problem.
+        if event.src_path == str(self._notebook_path):
+            self._debouncer.observe(event)
 
     def _handle_event(self, event):
         output_path = generate_html(self._notebook_path, self._output_dir)
         self._done_fn(self._notebook_path, output_path)
 
 
-class MacOSChangeHandler(FileSystemEventHandler):
-    def __init__(self, notebook_path: Path, output_dir: Path, done_fn: echo_generated):
-        self._notebook_path = notebook_path
-        self._output_dir = output_dir
-        self._done_fn = done_fn
+class Debouncer:
+    def __init__(self, callback_fn, debounce_time=0.1):
+        self._handler = callback_fn
+        self._debounce_time = debounce_time
+        self._last_events = {}
+        self._debounce_loop = True
+        self._lock = threading.Lock()
 
-    def on_modified(self, event):
-        # Used closed rather than modified because modified is called multiple times.
-        # I think the jupyter behavior is consistent in that it will open, write, close
-        # for every save.
-        if event.src_path == str(self._notebook_path):
-            output_path = generate_html(self._notebook_path, self._output_dir)
-            self._done_fn(self._notebook_path, output_path)
+        self._t = Thread(target=self.run, daemon=True)
+        self._t.start()
+
+    def observe(self, event):
+        at = time.time()
+        with self._lock:
+            self._last_events[event.src_path] = (event, at)
+
+    def run(self):
+        while self._debounce_loop:
+
+            # Collect events post-debounce.
+            now, to_process = time.time(), {}
+            with self._lock:
+                for path, (event, at) in self._last_events.items():
+                    if now - at > self._debounce_time:
+                        to_process[path] = event
+
+                # Remove events that can be processed within lock.
+                for path in to_process:
+                    del self._last_events[path]
+
+            # Process events (outside of lock)
+            for event in to_process.values():
+                self._handler(event)
+
+            time.sleep(self._debounce_time)
+
+    def stop(self):
+        self._debounce_loop = False
+        self._t.join()
--- tests/test_generate.py
+++ tests/test_generate.py
@@ -2,7 +2,7 @@ import shutil
 import time
 from pathlib import Path
 
-from falsifiable_nb.generate import SingleNotebookChangedHandler, MacOSChangeHandler
+from falsifiable_nb.generate import SingleNotebookChangedHandler
 from watchdog.observers import Observer
 
 
@@ -17,16 +17,14 @@ def test_jupyter_style_open_write_close_events(tmp_path):
     notebook_path = tmp_path / "001_basic.ipynb"
     copy_jupyter_style(basic_fixture, notebook_path)
 
-    time.sleep(0.1)
-
     observer = Observer()
 
     hits = []
     observer.schedule(
+        # TODO: does observer call stop() on this handler?
         SingleNotebookChangedHandler(
             notebook_path,
             tmp_path,  # as output dir
-            # printit
             lambda nb_path, output_path: hits.append((nb_path, output_path)),
         ),
         str(tmp_path),
@@ -34,13 +32,34 @@ def test_jupyter_style_open_write_close_events(tmp_path):
     )
     observer.start()
 
-    # This is ugly, but I don't know how to do it better.
-    time.sleep(0.1)
-    for _ in range(3):
-        copy_jupyter_style(basic_fixture, notebook_path)
-        time.sleep(0.1)
-    assert len(hits) == 3
+    # Hope initial write event propagated.
+    time.sleep(0.2)
+    hit_count = len(hits)
 
+    # ========================================================================
+    # This is ugly, but I don't know how to do it better.
+    # ========================================================================
+
+    # One write, one event.
+    copy_jupyter_style(basic_fixture, notebook_path)
+    for _ in range(10):
+        n = len(hits)
+        if n > hit_count:
+            assert n == hit_count + 1
+            hit_count = n
+            break
+        time.sleep(0.1)
+
+    # Two quick writes, one event.
+    copy_jupyter_style(basic_fixture, notebook_path)
+    copy_jupyter_style(basic_fixture, notebook_path)
+    for _ in range(10):
+        n = len(hits)
+        if n > hit_count:
+            assert n == hit_count + 1
+            hit_count = n
+            break
+        time.sleep(0.1)
     observer.stop()
     observer.join()
 
